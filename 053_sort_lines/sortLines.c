bf8521a83032af5cdca35594ff07e21d1c5ec50e
Shixing Cao <43682514+muxiazhixing@users.noreply.github.com>
2019-09-04 10:01:11 -0400

    Add files via upload

---
+++ 053_sort_lines/sortLines.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//This function is used to figure out the ordering
+//of the strings in qsort.  You do not need
+//to modify it.
+int stringOrder(const void * vp1, const void * vp2) {
+  const char * const * p1 = vp1;
+  const char * const * p2 = vp2;
+  return strcmp(*p1, *p2);
+}
+//This function will sort and print data (whose length is count).
+void sortData(char ** data, size_t count) {
+  qsort(data, count, sizeof(char *), stringOrder);
+}
+
+void sortandprint(FILE * f) {
+  char ** lines = NULL;
+  char * curr = NULL;
+
+  size_t sz;
+  size_t i = 0;
+
+  while (getline(&curr, &sz, f) >= 0) {
+    lines = realloc(lines, (i + 1) * sizeof(*lines));
+    lines[i] = curr;
+    curr = NULL;
+    i++;
+  }
+  free(curr);
+  sortData(lines, i);
+  for (size_t j = 0; j < i; j++) {
+    printf("%s", lines[j]);
+    free(lines[j]);
+  }
+  free(lines);
+}
+int main(int argc, char ** argv) {
+  //WRITE YOUR CODE HERE!
+
+  if (argc == 1) {
+    sortandprint(stdin);
+  }
+  else {
+    for (int i = 1; i < argc; i++) {
+      FILE * f = fopen(argv[i], "r");
+      if (f == NULL) {
+        fprintf(stderr, "cannot open file\n");
+        exit(EXIT_FAILURE);
+      }
+      sortandprint(f);
+      fclose(f);
+    }
+  }
+
+  return EXIT_SUCCESS;
+}
